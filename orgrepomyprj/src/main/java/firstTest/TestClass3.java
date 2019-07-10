b639c61f51611c602145407d301d1874b0f5943c
alessandro.garonzi <alealegar1985@gmail.com>
2019-07-10 12:06:35 +0200

    Otherchangelist2

---
+++ orgrepomyprj/src/main/java/firstTest/TestClass3.java
@@ -0,0 +1,38 @@
+package firstTest;
+
+public class TestClass3 {
+
+    public String c;
+    public String variable2;
+    public String d,e;
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public TestClass3() {
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public String getVariable2() {
+        return variable2;
+    }
+
+    public void setVariable2(String variable2) {
+        this.variable2 = variable2;
+    }
+
+
+
+}
